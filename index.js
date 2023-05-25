4dcd07b8bf656f48c3914ba5b4660efb65c56a8a
sipovalovdmitro <sipovalovdmitro@gmail.com>
2023-05-25 06:37:54 +0000

    simulate flashbots

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
-import { Wallet, BigNumber, ethers, providers } from "ethers";
-const {FlashbotsBundleProvider, FlashbotsBundleResolution} = require('@flashbots/ethers-provider-bundle');
-require("dotenv").config();
+import { Wallet, ethers } from "ethers";
+import {
+  FlashbotsBundleProvider,
+  FlashbotsBundleResolution
+} from "@flashbots/ethers-provider-bundle";
+import dotenv from "dotenv";
+dotenv.config();
 const privatekey = process.env.PRIVATE_KEY;
 
-// Setup the provider for sepolia or mainnet
-const provider = new providers.JsonRpcProvider('https://sepolia.infura.io/v3/1318c38c0a814d1fb072fc3d4b0002ce');
+// Setup the provider for goerli or mainnet
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://goerli.infura.io/v3/1318c38c0a814d1fb072fc3d4b0002ce"
+);
 
 // Create a unique flashbots id
 const authSigner = new Wallet(privatekey, provider);
 
 // Create the flashbots provider inside a start function
-const start = async()=>{
-    const flashbotsProvider = FlashbotsBundleProvider.create(
-        provider,
-        authSigner,
-        'https://relay-sepolia.flashbots.net'
-    )
-    // Setup the required gas and block variables
-    const GWEI = ethers.utils.parseUnits("1", "gwei");
-    const LEGACY_GAS_PRICE = GWEI.mul(12);
-    const PRIORITY_FEE = GWEI.mul(100);
-    const blockNumber = await provider.getBlockNumber();
-    const block = await provider.getBlock(blockNumber);
-    const maxBaseFeeInFutureBlock = FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(block.baseFeePerGas, 6);
-    const amountInEther = '0.001';
+const start = async () => {
+  const flashbotsProvider = await FlashbotsBundleProvider.create(
+    provider,
+    authSigner,
+    "https://relay-goerli.flashbots.net"
+  );
+  // Setup the required gas and block variables
+  const GWEI = ethers.utils.parseUnits("1", "gwei");
+  const LEGACY_GAS_PRICE = GWEI.mul(13);
+  const PRIORITY_FEE = GWEI.mul(100);
+  const blockNumber = await provider.getBlockNumber();
+  const block = await provider.getBlock(blockNumber);
+  const maxBaseFeeInFutureBlock =
+    FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(block.baseFeePerGas, 6);
+  const amountInEther = "0.001";
 
-    // Create the signed transfer transactions using both types
-    const signedTransactions = (await flashbotsProvider).signBundle([
-        {
-            signer: authSigner,
-            transaction: {
-                to: '0x501E809C8C8d268E136B6975b331EA398e07d35e',
-                type: 2,
-                maxFeePerGas: PRIORITY_FEE.add(maxBaseFeeInFutureBlock),
-                maxPriorityFeePerGas: PRIORITY_FEE,
-                data: '0x',
-                chainId: 11155111,
-                value: ethers.utils.parseEther(amountInEther),
-                // gasLimit: 300000,
-            }
-        },
-        // we need this second tx because flashbots only accept bundles that use at least 42000 gas.
-        {
-            signer: authSigner,
-            transaction: {
-                to: '0x501E809C8C8d268E136B6975b331EA398e07d35e',
-                type: 2,
-                gasPrice: LEGACY_GAS_PRICE,
-                data: '0x',
-                chainId: 11155111,
-                value: ethers.utils.parseEther(amountInEther),
-            }
-        }
-    ])
-}
+  // Create the signed transfer transactions using both types
+  const signedTransactions = await flashbotsProvider.signBundle([
+    {
+      signer: authSigner,
+      transaction: {
+        to: "0x501E809C8C8d268E136B6975b331EA398e07d35e",
+        type: 2,
+        maxFeePerGas: PRIORITY_FEE.add(maxBaseFeeInFutureBlock),
+        maxPriorityFeePerGas: PRIORITY_FEE,
+        data: "0x",
+        chainId: 5,
+        value: ethers.utils.parseEther(amountInEther)
+        // gasLimit: 300000,
+      }
+    },
+    // we need this second tx because flashbots only accept bundles that use at least 42000 gas.
+    {
+      signer: authSigner,
+      transaction: {
+        to: "0x501E809C8C8d268E136B6975b331EA398e07d35e",
+        gasPrice: LEGACY_GAS_PRICE,
+        data: "0x",
+        chainId: 5,
+        value: ethers.utils.parseEther(amountInEther)
+      }
+    }
+  ]);
 
+  // Run a flashbots simulation to make sure it works
+  console.log(new Date());
+  console.log("Starting to run the simulation...");
+  const simulation = await flashbotsProvider.simulate(
+    signedTransactions,
+    blockNumber + 1
+  );
+  console.log(new Date());
+  // Check the result of the simulation
+};
 
-
-start()
+start();

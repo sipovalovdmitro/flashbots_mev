89b6ff003f77483eca1d399a1194fa6dc60a4c9c
sipovalovdmitro <sipovalovdmitro@gmail.com>
2023-06-07 21:10:23 +0000

    update test

--- tasks/deploy.cjs
+++ tasks/deploy.cjs
@@ -1,7 +1,7 @@
 task("deploy", "Deploy the MEV contract", async (_taskArgs, hre) => {
   const signer = hre.ethers.provider.getSigner(0);
-  // const wethAddr = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // mainnet
-  const wethAddr = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; // goerli
+  const wethAddr = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // mainnet
+  // const wethAddr = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; // goerli
   console.log("Deployer address:", await signer.getAddress());
   const MEV = await ethers.getContractFactory("MEV");
   const mev = await MEV.deploy(wethAddr);
